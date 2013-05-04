1cf3e9fed5f52d8e118ac4de4a2df7ae657536d5
ch05042210@gmail.com <ch05042210@gmail.com@cd708c2f-6e57-2423-8232-08cc9cb70697>
2013-05-04 14:45:39 +0000

    [GM] redesign and reimplement!

--- firefox/youkuantiads.user.js
+++ firefox/youkuantiads.user.js
@@ -2,7 +2,7 @@
 // @name YoukuAntiADs
 // @author Harv
 // @description 视频去黑屏
-// @version 0.1.7.6
+// @version 0.2
 // @namespace http://userscripts.org/users/Harv
 // @updateURL https://userscripts.org/scripts/source/119622.meta.js
 // @downloadURL https://userscripts.org/scripts/source/119622.user.js
@@ -10,161 +10,105 @@
 // @include https://*/*
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
-
-/*
- * === 申明 ===
- * 本脚本使用的播放器虽然是本人修改制作的，但是使用了鲁夫v.opengg.me的内容
- * 这是因为在不久的以前优酷开始限制第三方播放器获取节目列表
- * 详情可以参见鲁夫的博文《万恶的优酷》（http://opengg.me/854/evil-youku/）
- *
- * 在这里感谢鲁夫提供的v.opengg.me以及他的优秀的作品！！！
- *
- * 同时，在这里说明一下制作这个播放器的目的，那就是为了记住播放历史
- * 如果您不建议有无历史记录，您可以直接使用鲁夫的脚本
- * OpenGG.Clean.Player（http://userscripts.org/scripts/show/120679）
- * 同样可以给您带来优质的体验
- *
- * 注：现在已不需要依赖鲁夫的服务器了！
- */
-
-/* History
- * 2013-4-21 v0.1.7.6 完善tudou外链支持（Firefox使用GM_xmlhttpRequest，其他浏览器使用YQL获取重定向后的URL）
- * 2013-4-14 v0.1.7.5 完善iqiyi外链规则，增加ku6外链支持
- * 2013-4-12 v0.1.7.4 完善tudou外链支持
- * 2013-4-2 v0.1.7.3 完善ku6，tudou规则
- * 2013-4-1 v0.1.7.2 完善ku6规则
- * 2013-3-31 v0.1.7.1 完善tudou外链
- * 2013-3-31 v0.1.7 增加对tudou支持（需要flashplayer11+）
- *                  增加对tudou外链不完美支持（需要安装脚本管理器，除Firefox外未测）
- *                  同时感谢15536900（http://bbs.kafan.cn/thread-1507278-1-1.html），以后播放器的更新以他为主
- * 2013-3-22 v0.1.6.1 增加iqiyi v5播放器（本身支持js脚本的浏览器已不再需要脚本管理器）
- * 2013-3-18 v0.1.6  完善youku规则，增加对iqiyi支持（各浏览器需要安装脚本管理器）
- * 2013-2-7 v0.1.5.1 修正css，修正opera兼容性
- * 2013-2-5 v0.1.5 多浏览器支持
- * 2012-11-19 v0.1.4 解决Fx17+不再支持CDATA引起的问题
- * 2012-10-25 v0.1.3 添加申明，感谢鲁夫提供的优酷节目列表转发功能
- * 2012-9-8 v0.1.2 解决百度贴吧视频不能显示的问题. example:http://tieba.baidu.com/p/1812024947
- * 2012-9-8 v0.1.1 googlecode 版本控制由 git 调整为 svn，现在应该不会白屏了.
- * 2012-9-1 v0.1 替换播放器调整，记住播放历史还是要的 @_@ !
- * 2012-8-17 v0.0.9 调整优酷swf匹配规则，同时替换播放器换成opengg.me
- * 2012-6-26 v0.0.8 使用 animationstart 事件代替 DOMNodeInserted. from http://opengg.me/784/high-performance-domnodeinserted-hack/
- * 2012-3-22 v0.0.7 使用网友CzBiX推荐的无广告完整功能player.cws.swf
- * 2012-2-15 v0.0.6 直接使用http://static.youku.com/v/swf/qplayer.swf替换swf播放器，不再需要获取swf版本号
- * 2012-1-7 v0.0.5 支持 DOMNodeInserted. example:http://live.taobao.com/detail/2011/12/22/567106/1.php?spm=1.51914.112571.4
- * 2011-12-20 v0.0.4 更改替换策略，使用prefs存贮swf版本号，定时更新（默认7天更新一次）
- * 2011-12-14 v0.0.3 更新
- * 2011-12-4 v0.0.2 增加对优酷外链的支持
- * 2011-12-3 v0.0.1 第一个版本
- */
-
-(function(document) {
-    var loader = 'https://haoutil.googlecode.com/svn/trunk/youku/loader.swf';
-    var ku6 = 'https://haoutil.googlecode.com/svn/trunk/player/ku6.swf';
-    var iqiyi = 'https://haoutil.googlecode.com/svn/trunk/player/iqiyi.swf';
-    var iqiyi5 = 'https://haoutil.googlecode.com/svn/trunk/player/iqiyi5.swf';
-    var tudou = 'https://haoutil.googlecode.com/svn/trunk/player/tudou.swf';
-    var tudou_olc = 'https://haoutil.googlecode.com/svn/trunk/player/testmod/olc_8.swf';
-    var tudou_sp = 'https://haoutil.googlecode.com/svn/trunk/player/testmod/sp.swf';
-    var players = {
-        'youku': {
-            find: /^http:\/\/static\.youku\.com(\/v[\d\.]+)?\/v\/swf\/(loader|q?player[^\.]*)\.swf/i,
-            replace: loader
-        },
-        'youku_out': {
-            find: /^http:\/\/player\.youku\.com\/player\.php\/.*sid\/([\w=]+).*\/v\.swf/i,
-            replace: loader + '?showAd=0&VideoIDS=$1'
-        },
-        'ku6': {
-            find: /^http:\/\/player\.ku6cdn\.com\/default\/.*\/\d+\/player\.swf/i,
-            replace: ku6
-        },
-        'ku6_out': {
-            find: /^http:\/\/player\.ku6\.com\/(inside|refer)\/([^\/]+)\/v\.swf.*/i,
-            replace: ku6 + '?vid=$2'
-        },
-        'iqiyi': {
-            find: /^http:\/\/www\.iqiyi\.com\/player\/\d+\/player\.swf/i,
-            replace: iqiyi
-        },
-        'iqiyi_out': {
-            find: /^http:\/\/(player|dispatcher)\.video\.i?qiyi\.com\/(.*[\?&]vid=)?([^\/&]+).*/i,
-            replace: iqiyi5 + '?vid=$3'
-        },
-        'tudou': {
-            find: /^http:\/\/js\.tudouui\.com\/.*player[^\.]*\.swf/i,
-            replace: tudou
-        },
-        'tudou_out': {
-            find: /^http:\/\/www\.tudou\.com\/.*\/v\.swf/i,
-            replace: tudou_olc + '?tvcCode=-1&swfPath=' + tudou_sp
+(function() {
+    Function.prototype.bind = function() {
+        var fn = this, args = Array.prototype.slice.call(arguments), obj = args.shift();
+        return function() {
+            return fn.apply(obj, args.concat(Array.prototype.slice.call(arguments)));
         }
     };
 
-    var done = [];
-
-    function reloadPlugin(elem) {
-        var nextSibling = elem.nextSibling;
-        var parentNode = elem.parentNode;
-        parentNode.removeChild(elem);
-        var newElem = elem.cloneNode(true);
-        done.push(newElem);
-        if(nextSibling) {
-            parentNode.insertBefore(newElem, nextSibling);
-        } else {
-            parentNode.appendChild(newElem);
-        }
-    }
-
-    function replace(elem) {
-        if(done.indexOf(elem) != -1) return;
-        done.push(elem);
-
-        var player = elem.data || elem.src;
-        if(!player) return;
-
-        var i, find, replace, isReplacing = false;
-        for(i in players) {
-            find = players[i].find;
-            if(find.test(player)) {
-                replace = players[i].replace;
-                
-                preHandle();
-
-                if(!isReplacing) {
-                    reallyReplace();
-                }
-                break;
+    function YoukuAntiAds() {};
+    YoukuAntiAds.prototype = {
+        _players: null,
+        _rules: null,
+        _done: null,
+        get players() {
+            if(!this._players) {
+                this._players = {
+                    'youku': 'https://haoutil.googlecode.com/svn/trunk/youku/loader.swf',
+                    'ku6': 'https://haoutil.googlecode.com/svn/trunk/player/ku6.swf',
+                    'iqiyi': 'https://haoutil.googlecode.com/svn/trunk/player/iqiyi.swf',
+                    'iqiyi5': 'https://haoutil.googlecode.com/svn/trunk/player/iqiyi5.swf',
+                    'tudou': 'https://haoutil.googlecode.com/svn/trunk/player/tudou.swf',
+                    'tudou_olc': 'https://haoutil.googlecode.com/svn/trunk/player/testmod/olc_8.swf',
+                    'tudou_sp': 'https://haoutil.googlecode.com/svn/trunk/player/testmod/sp.swf'
+                };
             }
-        }
-
-        function preHandle() {
-            if(i == 'iqiyi' && document.querySelector('span[data-flashplayerparam-flashurl]')) {
-                replace = iqiyi5;
-            } else if(i == 'tudou_out') {
-                isReplacing = true;
+            return this._players;
+        },
+        get rules() {
+            if(!this._rules) {
+                this._rules = {
+                    'youku': {
+                        'find': /^http:\/\/static\.youku\.com(\/v[\d\.]+)?\/v\/swf\/(loader|q?player[^\.]*)\.swf/i,
+                        'replace': this.players['youku']
+                    },
+                    'youku_out': {
+                        'find': /^http:\/\/player\.youku\.com\/player\.php\/.*sid\/([\w=]+).*(\/v)?\.swf.*/i,
+                        'replace': this.players['youku'] + '?showAd=0&VideoIDS=$1'
+                    },
+                    'ku6': {
+                        'find': /^http:\/\/player\.ku6cdn\.com\/default\/.*\/\d+\/player\.swf/i,
+                        'replace': this.players['ku6']
+                    },
+                    'ku6_out': {
+                        'find': /^http:\/\/player\.ku6\.com\/(inside|refer)\/([^\/]+)\/v\.swf.*/i,
+                        'replace': this.players['ku6'] + '?vid=$2'
+                    },
+                    'iqiyi': {
+                        'find': /^http:\/\/www\.iqiyi\.com\/player\/\d+\/player\.swf/i,
+                        'replace': this.players['iqiyi']
+                    },
+                    'iqiyi_out': {
+                        'find': /^http:\/\/(player|dispatcher)\.video\.i?qiyi\.com\/(.*[\?&]vid=)?([^\/&]+).*/i,
+                        'replace': this.players['iqiyi5'] + '?vid=$3'
+                    },
+                    'tudou': {
+                        'find': /^http:\/\/js\.tudouui\.com\/.*player[^\.]*\.swf/i,
+                        'replace': this.players['tudou']
+                    },
+                    'tudou_out': {
+                        'find': /^http:\/\/www\.tudou\.com\/.*\/v\.swf/i,
+                        'replace': this.players['tudou_olc'] + '?tvcCode=-1&swfPath=' + this.players['tudou_sp']
+                    }
+                }
+            }
+            return this._rules;
+        },
+        get done() {
+            if(!this._done) {
+                this._done = new Array();
+            }
+            return this._done;
+        },
+        initPreHandlers: function() {
+            this.rules['iqiyi']['preHandle'] = function(elem, find, replace, player) {
+                if(document.querySelector('span[data-flashplayerparam-flashurl]')) {
+                    replace = this.players['iqiyi5'];
+                }
+                this.reallyReplace.bind(this, elem, find, replace)();
+            }
+            this.rules['tudou_out']['preHandle'] = function(elem, find, replace, player) {
+                var fn = this;
                 var isFx = /firefox/i.test(navigator.userAgent);
                 GM_xmlhttpRequest({
                     method: isFx ? 'HEAD' : 'GET',
-                    url: isFx ? player : 'http://query.yahooapis.com/v1/public/yql?q=use "http://javarants.com/yql/javascript.xml" as j; select * from j where code="response.object = y.rest(\'' + player + '\').followRedirects(false).get().headers.location;"&format=json',
+                    url: isFx ? player : 'http://query.yahooapis.com/v1/public/yql?q=use "http://javarants.com/yql/javascript.xml" as j; select * from j where code="response.object = y.rest(\'' + encodeURIComponent(player) + '\').followRedirects(false).get().headers.location;"&format=json',
                     onload: function(response) {
                         var match = (isFx ? response.finalUrl : response.responseText).match(/(iid|youkuid|resourceid|autoplay)=[^&]+/ig);
-                        if(match) replace += '&' + match.join('&');
-                        reallyReplace();
+                        if(match) {
+                            replace += '&' + match.join('&');
+                            fn.reallyReplace.bind(fn, elem, find, replace)();
+                        }
                     }
                 });
             }
-        }
-
-        function reallyReplace() {
-            elem.data && (elem.data = elem.data.replace(find, replace)) || elem.src && ((elem.src = elem.src.replace(find, replace)) && (elem.style.display = 'block'));
-            reloadPlugin(elem);
-        }
-    }
-    
-    function addAnimations() {
-        var style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = 'object,embed{\
+        },
+        addAnimations: function() {
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = 'object,embed{\
 -webkit-animation-duration:.001s;-webkit-animation-name:playerInserted;\
 -ms-animation-duration:.001s;-ms-animation-name:playerInserted;\
 -o-animation-duration:.001s;-o-animation-name:playerInserted;\
@@ -173,21 +117,65 @@ animation-duration:.001s;animation-name:playerInserted;}\
 @-ms-keyframes playerInserted{from{opacity:0.99;}to{opacity:1;}}\
 @-o-keyframes playerInserted{from{opacity:0.99;}to{opacity:1;}}\
 @keyframes playerInserted{from{opacity:0.99;}to{opacity:1;}}';
-      
-        document.getElementsByTagName('head')[0].appendChild(style);
-    }
-    
-    function animationsHandler(e) {
-        if(e.animationName === 'playerInserted') {
-            replace(e.target);
-        }
-    }
-    
-    document.body.addEventListener('webkitAnimationStart', animationsHandler, false);
-    document.body.addEventListener('msAnimationStart', animationsHandler, false);
-    document.body.addEventListener('oAnimationStart', animationsHandler, false);
-    document.body.addEventListener('animationstart', animationsHandler, false);
-  
-    addAnimations();
+            document.getElementsByTagName('head')[0].appendChild(style);
+        },
+        animationsHandler: function(e) {
+            if(e.animationName === 'playerInserted') {
+                this.replace(e.target);
+            }
+        },
+        replace: function(elem) {
+            if(this.done.indexOf(elem) != -1) return;
+            this.done.push(elem);
 
-})(window.document);
+            var player = elem.data || elem.src;
+            if(!player) return;
+
+            var i, find, replace, isReplacing = false;
+            for(i in this.rules) {
+                find = this.rules[i]['find'];
+                if(find.test(player)) {
+                    replace = this.rules[i]['replace'];
+                    if('function' === typeof this.rules[i]['preHandle']) {
+                        isReplacing = true;
+                        this.rules[i]['preHandle'].bind(this, elem, find, replace, player)();
+                    }
+                    if(!isReplacing) {
+                        this.reallyReplace.bind(this, elem, find, replace)();
+                    }
+                    break;
+                }
+            }
+        },
+        reallyReplace: function(elem, find, replace) {
+            elem.data && (elem.data = elem.data.replace(find, replace)) || elem.src && ((elem.src = elem.src.replace(find, replace)) && (elem.style.display = 'block'));
+            this.reloadPlugin(elem);
+        },
+        reloadPlugin: function(elem) {
+            var nextSibling = elem.nextSibling;
+            var parentNode = elem.parentNode;
+            parentNode.removeChild(elem);
+            var newElem = elem.cloneNode(true);
+            this.done.push(newElem);
+            if(nextSibling) {
+                parentNode.insertBefore(newElem, nextSibling);
+            } else {
+                parentNode.appendChild(newElem);
+            }
+        },
+        init: function() {
+            this.initPreHandlers();
+
+            var handler = this.animationsHandler.bind(this);
+
+            document.body.addEventListener('webkitAnimationStart', handler, false);
+            document.body.addEventListener('msAnimationStart', handler, false);
+            document.body.addEventListener('oAnimationStart', handler, false);
+            document.body.addEventListener('animationstart', handler, false);
+
+            this.addAnimations();
+        }
+    };
+
+    new YoukuAntiAds().init();
+})();
